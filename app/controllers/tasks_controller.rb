f4b5c8d314cd6612b2e625dddf195ac20ea89f84
Kevin Rietsch <kevin.rietsch@gmail.com>
2019-12-14 16:39:56 +0100

    rails task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,27 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task.completed ? @msg = 'This task is completed' : @msg = 'This task is not completed yet'
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create!(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
